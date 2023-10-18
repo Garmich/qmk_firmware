128b9ed8b15bac46918b4e5156f043b21dc6a727
mgarcia6 <michael.garciar@outlook.com>
2023-10-18 12:42:23 +0200

    Añadida capa de layaout QWERTY

--- keyboards/crkbd/rev1/keymaps/garmich/keymap.c
+++ keyboards/crkbd/rev1/keymaps/garmich/keymap.c
@@ -16,11 +16,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
     [GAME_LY] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX,    KC_R,    KC_Q,    KC_W,    KC_E, XXXXXXX,                      TD(DOT),    KC_P,    KC_T,    KC_M,    KC_K, XXXXXXX,
+      XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_F,    KC_A,    KC_S,    KC_D,    KC_G,                         KC_B,    KC_R,    KC_S,    KC_N,    KC_D,    KC_X,
+      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,                         KC_F,    KC_C,     TIL,     CTL,    KC_H, KC_CAPS,
+      XXXXXXX, KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,                         KC_B,    KC_N,    KC_M, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX, XXXXXXX, KC_SPC,    TG(BASE_LY), XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
